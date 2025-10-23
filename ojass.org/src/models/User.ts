2405968bb14c18d76fe0ae0fb33a03347b07cfeb
Pranaw Kumar <177040319+prana-W@users.noreply.github.com>
2025-10-23 22:06:10 +0530

    Add User model

---
+++ ojass.org/src/models/User.ts
@@ -0,0 +1,65 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    phone: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    idCardImage: {
+        type: String // Cloudinary URL
+    },
+
+    isEmailVerified: {
+        type: Boolean,
+        default: false
+    },
+
+    // Otp and Otp expiration for email verification
+    emailOtp: {
+        type: Number,
+    },
+    emailOtpExpires: {
+        type: Date
+    },
+
+    // Otp and Otp expiration for reset password
+    resetPasswordOtp: {
+        type: Number
+    },
+    resetPasswordOtpExpires: {
+        type: Date
+    },
+
+    // Transaction Related Fields
+    orderId: {
+        type: String,
+    },
+    razorpayPaymentId: {
+        type: String,
+    },
+    razorpayOrderId: {
+        type: String,
+    },
+    razorpaySignature: {
+        type: String,
+    }
+
+}, { timestamps: true });
+
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+
+export default User;
