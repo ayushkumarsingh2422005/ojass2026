0bc3b74f9084604c634c221c88c58b49b15f183f
usrjsr <utkarshsinghdau@gmail.com>
2025-11-07 15:44:47 +0530

    Added Timeline Page

--- ojass.org/src/app/timeline/page.tsx
+++ ojass.org/src/app/timeline/page.tsx
@@ -1,12 +1,12 @@
 "use client";
-import TimelineDial from "@/components/OverlayLayout/TimelineDial";
+import TimelinePage from "@/components/Timeline";
 import React from "react";
 
 export default function TimelineDialWrapper() {
   return (
     <div>
-        <div className="mt-[150px] h-[400px] w-[600px] mx-auto  bg-amber-500 flex "></div>
-   <TimelineDial/>
+   {/* <TimelineDialPage/> */}
+   <TimelinePage/>
     </div>
   );
 }
